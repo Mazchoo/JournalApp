00946a2f72786ccd7ca620cbd79434acd865126a
Matthew Mitchell <mazchoo@gmail.com>
2023-01-07 23:43:12 +0100

    Added a todo

--- static/JS/entry.paragraph.js
+++ static/JS/entry.paragraph.js
@@ -50,7 +50,7 @@ let editParagraphWhenInitialised = function(updateInd, paragraphContent, counter
     if (!editParagraphContent(updateInd, paragraphContent)) {
         counter--;
         // ToDo - try to fix this issue by separating this initialisation from creating the html components
-        setTimeout(editParagraphWhenInitialised, 1500 * (100 / counter), updateInd, paragraphContent, counter);
+        setTimeout(editParagraphWhenInitialised, 1000 * (100 / counter), updateInd, paragraphContent, counter);
     }
 }
 
