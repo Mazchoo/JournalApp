88a1deb0478d79f56b7eb464db31fdd2ebf79755
Matthew Mitchell <mazchoo@gmail.com>
2024-01-27 16:18:06 +0700

    Added automatic addition of security file

--- manage.py
+++ manage.py
@@ -4,6 +4,35 @@ import os
 import sys
 from pathlib import Path
 from typing import List
+import re
+import json
+
+from django.core.management.utils import get_random_secret_key
+
+USERNAME_REGEX = '^[A-Za-z_][A-Za-z0-9_]*'
+
+
+def generateSecurityDict(username: str, password: str):
+    security_dict = {
+        "ADMIN_USERNAME": username,
+        "ADMIN_PASSWORD": password,
+        "SECRET_KEY": f"django-insecure-{get_random_secret_key()}"
+    }
+    with open(f"{os.getcwd()}/security.json", "w") as f:
+        json.dump(security_dict, f)
+
+
+def addSecurityKey():
+    print("No security details detected")
+    username = ""
+    while not re.search(USERNAME_REGEX, username):
+        username = input("Create a valid username :")
+
+    password = ""
+    while len(password) < 8:
+        password = input("Create a password with 8 characters or more :")
+
+    generateSecurityDict(username, password)
 
 
 def main(command: List[str]):
@@ -21,7 +50,12 @@ def main(command: List[str]):
 
 
 if __name__ == '__main__':
-    ''' Ensure database exists before running server. '''
+    ''' Ensure security details exist. '''
+    if not Path(f"{os.getcwd()}/security.json").exists():
+        addSecurityKey()
+
+    ''' Ensure database exists. '''
     if not Path(f"{os.getcwd()}/db.sqlite3").exists():
         main([__file__, "migrate"])
+
     main(sys.argv)
