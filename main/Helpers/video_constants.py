3ce937958650c3d74788baa762be0ff2311c1e44
Matthew Mitchell <mazchoo@gmail.com>
2024-12-31 12:26:57 +0700

    Tweaked video display

--- main/Helpers/video_constants.py
+++ main/Helpers/video_constants.py
@@ -4,10 +4,10 @@ class VideoConstants:
     supported_extensions: Tuple[str] = ('.mp4', )
     save_image_extention: str = 'jpeg'
     reserved_video_tags: tuple = ("_icon", "_resized")
-    collage_image_longest_side: int = 512
+    collage_image_longest_side: int = 768
     collage_nr_rows: int = 1
     collage_nr_cols: int = 3
     nr_extra_cols: int = 1
-    billateral_filter: bool = True
+    billateral_filter: bool = False
     collage_spacing: int = 10
     icon_size: int = 96
--- main/forms.py
+++ main/forms.py
@@ -57,7 +57,7 @@ class ImageForm(ModelForm):
         source_file_obj = Path(source_path)
 
         if not target_file_obj.exists() and not source_file_obj.exists():
-            raise forms.ValidationError(f"Cannot find '{file_name}' in /Images folder.")
+            raise forms.ValidationError(f"Cannot find '{file_name}' in ./Entries folder.")
 
         if target_file_obj.suffix.lower() not in ImageConstants.supported_extensions:
             message = f"Extension '{target_file_obj.suffix}' is not a recognised image extension"
